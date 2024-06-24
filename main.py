6550e8ef6957fa4ee0bdd8da5c0937799d3e9c4d
Providence <111455427+directlypro@users.noreply.github.com>
2024-06-24 19:23:27 +0200

    reverse geocode function added

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-from geopy.geocoders import Nominatim 
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderServiceError
+import folium
 
-def zip_code():
-    place = input('Enter Place :')
+def get_location_info(place):
+    try:
+        geolocator = Nominatim(user_agent="abcd")
+        location = geolocator.geocode(place)
+        
+        if location:
+            data = location.raw
+            loc_data = data['display_name'].split()
+            print('Full Location:', location.address)
+            print('Latitude:', location.latitude)
+            print('Longitude:', location.longitude)
+            print('Zip Code:', loc_data[-3])
+        else:
+            print("Location not found.")
+    except GeocoderServiceError as e:
+        print("Geocoding error:", e)
 
-    geolocator = Nominatim(user_agent="abcd")
-    location = geolocator.geocode(place)
+def reverse_geocode(lat, lon):
+    try:
+        geolocator = Nominatim(user_agent="abcd")
+        location = geolocator.reverse((lat, lon))
+        
+        if location:
+            print('Full Location:', location.address)
+            print('Latitude:', location.latitude)
+            print('Longitude:', location.longitude)
+        else:
+            print("Location not found.")
+    except GeocoderServiceError as e:
+        print("Geocoding error:", e)
 
-    data = location.raw
-    loc_data = data['display_name'].split()
-    print('Full Location')
-    print(loc_data)
-    print("Zip Code :", loc_data[-3])
+def visualize_location(lat, lon):
+    map_ = folium.Map(location=[lat, lon], zoom_start=13)
+    folium.Marker([lat, lon], popup="Location").add_to(map_)
+    return map_
 
-zip_code()
+def main():
+    choice = input("Enter '1' for place name or '2' for coordinates: ")
+    if choice == '1':
+        place = input('Enter Place: ')
+        get_location_info(place)
+    elif choice == '2':
+        lat = float(input('Enter Latitude: '))
+        lon = float(input('Enter Longitude: '))
+        reverse_geocode(lat, lon)
+        map_ = visualize_location(lat, lon)
+        map_.save('location.html')
+        print("Map saved as 'location.html'. Open this file in a browser to view the location.")
+    else:
+        print("Invalid choice.")
 
+if __name__ == "__main__":
+    main()
